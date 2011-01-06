02d56b13b73d50e529118d6495a73823379c673e
Felipe Oliveira <felipe.oliveira@abril.com.br>
2011-01-06 13:48:47 -0200

    changed visibility of Configuration class

--- src/Boycott/Configuration.cs
+++ src/Boycott/Configuration.cs
@@ -4,7 +4,7 @@
     using System.IO;
     using Boycott.Provider;
 
-    static internal class Configuration {
+    public static class Configuration {
         static Configuration() {
             NamingProvider = GetProvider(new NamingProvider());
         }
